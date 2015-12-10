0a83363b8082381fec63a4e7cdc73be3cbc5ee84
Kamil Monticolo <kmonticolo+github@gmail.com>
2015-12-10 14:55:13 +0100

    Create testcvs.sh
    script for test speed of ANONCVS servers response for OpenBSD

---
+++ testcvs.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# 10.12.15 kmonti
+# script for test speed of ANONCVS servers response for OpenBSD
+
+ftp -o - http://www.openbsd.org/anoncvs.html |grep CVSROOT= |sed -e 's/^.*@//g' -e 's/:.*$//g' >serwery 2>/dev/null
+#curl http://www.openbsd.org/anoncvs.html |grep CVSROOT= |sed -e 's/^.*@//g' -e 's/:.*$//g' >serwery
+wait=`cat serwery|wc -l`
+echo -n "wait" `echo "( $wait * 3 )"| bc` "seconds."
+for i in `cat serwery`;do j=`ping -c3 $i|grep round| cut -d \/ -f 5|cut -f1 -d .`; \
+echo -n $i" "$j >>pingidoserwerow; echo -n .; echo >>pingidoserwerow;done
+#grep round pingidoserwerow |sed -e s'/\//\;/g' -e 's/\./,/g'
+
+cat pingidoserwerow |grep [0-9]$ | sort -nk 2
+#EOF
