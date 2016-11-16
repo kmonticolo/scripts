f9d5780e5c86c3a9772e15cba9ae04c795ebb82a
Kamil Monticolo <kmonticolo+github@gmail.com>
2016-11-16 08:13:15 +0100

    Update geonaute.sh

--- geonaute.sh
+++ geonaute.sh
@@ -3,9 +3,9 @@
 
 rm *thm 2>/dev/null
 for file in FILE[0-9][0-9][0-9][0-9].mp4; do
-  mc=`ls -l |grep $file|awk {'print $7'}\n`
-  day=`ls -l |grep $file|awk {'print $8'}\n`
-  time=`ls -l |grep $file|awk {'print $9'}\n`
+  mc=`ls -l |grep $file|awk {'print $7'}`
+  day=`ls -l |grep $file|awk {'print $8'}`
+  time=`ls -l |grep $file|awk {'print $9'}|sed 's/:/./'`
   oldname=`echo $file|sed 's/.mp4//g'`
   newname=${oldname}-${mc}-${day}-${time}.mp4
   #echo $newname
