8061bfd92c7e477f68b0f9f4dd7535afbd899837
Kamil Monticolo <kmonticolo+github@gmail.com>
2018-02-27 09:04:52 +0100

    Update scptest.sh

--- scptest.sh
+++ scptest.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 TMP=$(mktemp)
+FILE=/tmp/100m
+TARGET=seachange@192.168.113.49
 #date
-[[ -r /tmp/100m ]] || dd if=/dev/zero of=/tmp/100m bs=1M count=100
-time (scp /tmp/100m  seachange@192.168.113.49:/tmp/ ) 2>$TMP 1>/dev/null
-TIME=`awk -F'[ ms]+' '/^real/ {print "copy time: "$2"s"}' $TMP`
-SPEED=`awk -F'[ ms]+' '/^real/ {print "avg speed: "100/$2"MBps"}' $TMP`
-echo $(date +%F-%H:%M) $TIME $SPEED
-rm $TMP
+[[ -r "$FILE" ]] || dd if=/dev/zero of="$FILE" bs=1M count=100
+time (scp "$FILE"  "$TARGET":/tmp/ ) 2>"$TMP" 1>/dev/null
+TIME=`awk -F'[ ms]+' '/^real/ {print "copy time: "$2"s"}' "$TMP"`
+SPEED=`awk -F'[ ms]+' '/^real/ {print "avg speed: "100/$2"MBps"}' "$TMP"`
+echo $(date +%F-%H:%M) "$TIME" "$SPEED"
+rm "$TMP"
